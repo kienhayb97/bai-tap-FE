2a501fc95cb1314030f11f4c99f6caf2271326d7
ThinkPad-T450s <kienhayb97>
2019-12-17 16:18:41 +0700

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {CreateUserComponent} from './baitap-FE/user/create-user/create-user.component';
+import {LoginUserComponent} from './baitap-FE/user/login-user/login-user.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'auth/login',
+    component: LoginUserComponent
+  },
+  {
+    path: 'registered',
+    component: CreateUserComponent
+  },
+  {
+    path: 'auth/login/registered',
+    component: CreateUserComponent
+  },
+];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateUserComponent } from './baitap-FE/user/create-user/create-user.component';
+import { AppRoutingModule } from './app-routing.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule} from '@angular/router';
+import { LoginUserComponent } from './baitap-FE/user/login-user/login-user.component';
+import { HomeComponent } from './baitap-FE/component/home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateUserComponent,
+    LoginUserComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
